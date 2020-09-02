ed8430d0318e91848cc88aa5d5d15eec1679e642
Ana Claudia Vignola <ana.vignola@b4b.ventures>
2020-09-02 04:14:32 -0300

    change favicon test

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#E5E9F0`,
         theme_color: `#E5E9F0`,
         display: `minimal-ui`,
-        icon: `/src/images/favicon.ico`, // This path is relative to the root of the site.
+        icon: `/src/images/camera2.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sitemap`,
